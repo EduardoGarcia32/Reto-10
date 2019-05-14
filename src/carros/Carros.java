4fb6d829f7d16f917a542dcc791f69efc2b1e440
EduardoGarcia32 <slender560@gmail.com>
2019-05-14 14:11:33 -0500

    Creacion de clases

---
+++ src/carros/Carros.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carros;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Eduardo Garcia
+ */
+public class Carros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int opcion = 0;
+        Scanner ent = new Scanner(System.in);
+        Llanta llantas_auto[] =  new Llanta[4];
+        Renta rentas[] = new Renta[20];
+        ArrayList<Renta> listaRenta = new ArrayList<Renta>();
+        ArrayList<Automovil> listaAutomoviles = new ArrayList<Automovil>();
+
+    }
+    
+}
